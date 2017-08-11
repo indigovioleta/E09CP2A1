2d2e3f08ee1843f3350ff18a364814175c1fa282
Eva Guajardo <guajardo.eva@gmail.com>
2017-08-10 21:36:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ $.ajax({
 });
 
 $('#btn-peso').on('click', function(){
-	/*$('#dolar-input').val($('#peso-input').val() / dailyIndicators.dolar.valor.toFixed(2));*/
 	$('#peso').text('CLP $ '+$('#peso-input').val());
 	$('#dolar').text('US $ '+$('#peso-input').val()/dailyIndicators.dolar.valor.toFixed(2));
  	$('#euro').text('€ '+$('#peso-input').val()/dailyIndicators.euro.valor.toFixed(2));
@@ -23,10 +22,8 @@ $('#btn-peso').on('click', function(){
 });
 
 $('#btn-dolar').on('click', function(){
-	/*$('#peso-input').val($('#dolar-input').val() * dailyIndicators.dolar.valor.toFixed(2));*/
 	$('#peso').text('CLP $ '+$('#dolar-input').val()*dailyIndicators.dolar.valor.toFixed(2));
 	$('#dolar').text('US $ '+$('#dolar-input').val());
- 	/*$('#euro').text('€ '+$('#dolar-input').val()*dailyIndicators.euro.valor.toFixed(2));*/
  	$('#table-euro').hide();
  	$('#table-uf').hide();
  	$('#table-utm').hide();
